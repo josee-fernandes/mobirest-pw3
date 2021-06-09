1f03c75f5d0b42005b8a7623687d33cda9e0c10d
josee-fernandes <jose-vitor-7@hotmail.com>
2021-06-09 20:03:13 -0300

    Update company / job controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(array('prefix' => 'api'), function () {
+    
+    Route::get('/', function () {
+        return response()->json(['message' => 'Companies & Jobs API', 'status' => 'Connected']);;
+    });
+
+    Route::resource('jobs', 'JobController');
+    
+    Route::resource('companies', 'CompanyController');
+});
+
+Route::get('/', function () {
+    return redirect('/api');
+});
